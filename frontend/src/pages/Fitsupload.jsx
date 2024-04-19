0e5e81e832c08b24c451db4eff561654b202ffaf
jeffinsamgeorge7 <jeffinsamrock@gmail.com>
2024-04-19 16:20:57 +0530

    update

--- frontend/src/pages/Fitsupload.jsx
+++ frontend/src/pages/Fitsupload.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { purple } from '@mui/material/colors';
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 
 const Fitsupload = () => {
   const [file, setFile] = useState(null);  
@@ -8,6 +15,14 @@ const Fitsupload = () => {
   const [imageSrcs, setImageSrcs] = useState([]);
   const [loading, setLoading] = useState(true); // Add a loading state
   const [refreshKey, setRefreshKey] = useState(0); // Add a refresh key
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
 
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -21,7 +36,7 @@ const Fitsupload = () => {
       const response = await axios.post('http://localhost:8000/api/upload/', formData);
       if (response.status === 201) {
         setImageUrl(true); // Set image URL if upload is successful
-        setResponseText('Image uploaded successfully!');
+        setResponseText('File uploaded successfully!');
         console.log(response.data); // Print the response data in the console
         setRefreshKey(oldKey => oldKey + 1); // Increment the refresh key to trigger a re-render
       } else {
@@ -52,16 +67,40 @@ const Fitsupload = () => {
 
   return (
     <div>
-      <input type="file" onChange={onFileChange} />
+      <h1>TESS Lightkurve Analyser</h1>
+       <FormControl sx={{ m: 1, width: '400px' }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-weight"
+            endAdornment={<InputAdornment position="end"><DriveFolderUploadIcon /></InputAdornment>}
+            aria-describedby="outlined-weight-helper-text"
+            onChange={onFileChange} 
+            type='file'
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+         
+        </FormControl>
+
+        <ColorButton variant="contained"  style={{marginTop:"20px"}} onClick={onFileUpload}>Submit</ColorButton>
+      {/* <input type="file" onChange={onFileChange} />
       <button onClick={onFileUpload}>Upload!</button>
-      {imageUrl && <img src={imageUrl} alt="Uploaded" />}
+      {imageUrl && <img src={imageUrl} alt="Uploaded" />} */}
       <p>{responseText}</p>
       {!loading && imageUrl && 
         <div>
+          <p>The Transiting Exoplanet Survey Satellite (TESS) is a NASA-sponsored Astrophysics Explorer-class mission that is performing a near all-sky survey to search for planets transiting nearby stars.</p>
+           <p>
+           Simple Aperture Photometry (SAP): The SAP light curve is calculated by summing together the brightness of pixels that fall within an aperture set by the TESS mission. This is often referred to as the optimal aperture, but in spite of its name, it can sometimes be improved upon! Because the SAP light curve is a sum of the brightness in chosen pixels, it is still subject to systematic artifacts of the mission.
+           </p><p>
+Pre-search Data Conditioning SAP flux (PDCSAP) flux: SAP flux from which long term trends have been removed using so-called Co-trending Basis Vectors (CBVs). PDCSAP flux is usually cleaner data than the SAP flux and will have fewer systematic trends.
+</p>
+
+         
           <img src={imageSrcs[0]} alt="From server 1" />
-          <p>Your paragraph text goes here.</p>
+          <h1>Flattening Lightkurve</h1>
           <img src={imageSrcs[1]} alt="From server 2" />
-          <p>Your paragraph text goes here.</p>
+         <h1>Binning the light curve</h1>
           <img src={imageSrcs[2]} alt="From server 3" />
         </div>
       }
--- frontend/src/pages/Skyview.jsx
+++ frontend/src/pages/Skyview.jsx
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { purple } from '@mui/material/colors';
 
 const ImageGenerator = () => {
   const [ra, setRa] = useState('');
   const [dec, setDec] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,16 +27,34 @@ const ImageGenerator = () => {
 
   return (
     <div>
+      <h1>Celestial Map</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          RA:
-          <input type="text" value={ra} onChange={(e) => setRa(e.target.value)} />
-        </label>
-        <label>
-          DEC:
-          <input type="text" value={dec} onChange={(e) => setDec(e.target.value)} />
-        </label>
-        <button type="submit">Generate Image</button>
+
+      <FormControl sx={{ m: 1, width: '400px' }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-weight"
+            value={ra} onChange={(e) => setRa(e.target.value)}
+            aria-describedby="outlined-weight-helper-text"
+            placeholder='RA'
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: '400px' }} variant="outlined">
+          <OutlinedInput
+            id="outlined-adornment-weight"
+            value={dec} onChange={(e) => setDec(e.target.value)} 
+            aria-describedby="outlined-weight-helper-text"
+            placeholder='DEC'
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+        </FormControl>
+
+        <ColorButton variant="contained"  style={{marginTop:"20px"}}  type="submit">Submit</ColorButton>
       </form>
       {imageUrl && <img src={imageUrl} alt="Generated" />}
     </div>
