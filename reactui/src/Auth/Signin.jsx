92c6697bf73325df0f09831dfee38a999929355c
JEFFIN SAM GEORGE <jeffinsamrock@gmail.com>
2024-05-12 20:44:03 +0530

    update

--- reactui/src/Auth/Signin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Signin = () => {
-  return (
-    <div>Signin
-      <p>ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddpfffffffffffffffffffffffffffffffff</p>
-    </div>
-  )
-}
